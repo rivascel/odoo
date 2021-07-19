ccf214f43ea1e3e8abdafbd66fb06ced1df6a252
rivascel <rivascel@gmail.com>
2021-07-19 18:41:22 -0500

    Add files via upload

--- ideas_app/models/califica.py
+++ ideas_app/models/califica.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models,api, exceptions
 from types import *
 from datetime import datetime, time
 
 class califica_ideas (models.Model):   
 	_name = "califica.ideas"
-	__inherit = 'registro'
+	#__inherit = 'registro'
 
 	idea_id = fields.Many2one('registro.ideas','codigo')
 	calificacion_id = fields.Float('calificacion', size=10, required=True)
-	fecha_actual = fields.Datetime('Fecha Calificacion', default=datetime.now(), required=True, readonly=True)
+	fecha_actual = fields.Datetime('Fecha Calificacion', default=datetime.now(), readonly=True)
 	usuario_id= fields.Many2one('res.partner', 'id usuario', required=True)
-	votos = fields.Integer('Votos', size=20, required=True, readonly=True)
-	promedio = fields.Float('Promedio',size=10,required=True, readonly=True)
+	
+	@api.constrains('calificacion_id')
+	def _calif(self):
+		for record in self:
+			if (record.calificacion_id < 0) or (record.calificacion_id > 10):
+				raise exceptions.ValidationError('La calificación debe ser entre 0 y 10')
+				
+
+
 
 
--- ideas_app/models/registro.py
+++ ideas_app/models/registro.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import models, fields, api 
 from datetime import time, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
@@ -9,22 +9,34 @@ class registro_ideas(models.Model):
 	
 	name = fields.Char('Nombre de idea', size=256, required=True) 
 	codigo = fields.Integer('código', size=20, required=True)
-	grupo = fields.Char('Grupo', size=100, required=True)
-	descripcion = fields.Char('Descripcion', size=200, required=True)
+	grupo = fields.Char('Grupo', size=100)
+	descripcion = fields.Char('Descripcion', size=200)
 	fechainicial = fields.Datetime('Fecha Inicial', size=20, required=True)
-	fechafinal = fields.Datetime('Fecha Final', required=True, compute='set_calculo_fecha')
 	
 	@api.depends('fechainicial')
-	def set_calculo_fecha(self):
+	def _set_calculo_fecha(self):
 		for rec in self:
-			if rec.fechainicial:
-				rec.fechafinal = '01/11/2020'
-			else:
-				rec.fechafinal = '01/11/2021'
+			rec.fechafinal = rec.fechainicial+timedelta(days=30)
+			
+	fechafinal = fields.Datetime('Fecha Final', compute='_set_calculo_fecha')
+	votos = fields.Integer('Votos', size=20, readonly=True, compute='_calc_voto')
+	promedio = fields.Float('Promedio',size=10, readonly=True)
+
+	#sumar por cada idea las calificaciones obtenidas y promediar
+	def _calc_voto(self):
+		for rec in self.env['califica.ideas'].search([]):
+			domain=[
+				('name','=',rec.idea_id.id)
+					]
+			records = self.env['califica.ideas'].search(domain)
+			scored = records.mapped('calificacion_id')
+			rec.average=sum(scored)/len(rec)
 
 
-	votos = fields.Integer('Votos', size=20, required=True, readonly=True)
-	promedio = fields.Float('Promedio',size=10,required=True, readonly=True)
+
+    		
+    				
+    		
 
 
 
