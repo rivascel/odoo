2a7b92ab0e6da1e5593cdc126d23fe2a51c41f51
rivascel <rivascel@gmail.com>
2021-07-22 21:59:33 -0500

    Add files via upload

--- ideas_app/models/califica.py
+++ ideas_app/models/califica.py
@@ -7,18 +7,23 @@ class califica_ideas (models.Model):
 	_name = "califica.ideas"
 	#__inherit = 'registro'
 
-	idea_id = fields.Many2one('registro.ideas','codigo')
+	name = fields.Many2one('registro.ideas','Idea')
+	codigo = fields.Integer(string='codigo', related='name.codigo', store=True)
 	calificacion_id = fields.Float('calificacion', size=10, required=True)
 	fecha_actual = fields.Datetime('Fecha Calificacion', default=datetime.now(), readonly=True)
-	usuario_id= fields.Many2one('res.partner', 'id usuario', required=True)
+	usuario_id= fields.Many2one('res.users', 'Usuario', readonly=True, default=lambda self: self.env.user)
 	
 	@api.constrains('calificacion_id')
 	def _calif(self):
 		for record in self:
 			if (record.calificacion_id < 0) or (record.calificacion_id > 10):
 				raise exceptions.ValidationError('La calificación debe ser entre 0 y 10')
-				
+	
+	#Se genene una restriccion para que el usuario solo puede calificar una sola vez una Idea 
+	@api.constrains('calificacion_id')
+	def _validaVoto(self):
+		for rec in self:
+			usuario_ideas_ids = self.search(['&',('usuario_id','=',rec.create_uid),('codigo','=',rec.codigo)])
 
-
-
-
+			if 'codigo' in 'usuario_ideas_ids' and 'usuario_id' in 'usuario_ideas_ids':
+				raise exceptions.ValidationError('Ya calificó esta idea')
--- ideas_app/models/registro.py
+++ ideas_app/models/registro.py
@@ -1,48 +1,41 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api 
-from datetime import time, datetime, timedelta
+from odoo import models, fields, api, exceptions
+from datetime import date, time, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
+
 class registro_ideas(models.Model):
-	_name = "registro.ideas"
-	_description = "Tabla de ideas"
-	
-	name = fields.Char('Nombre de idea', size=256, required=True) 
-	codigo = fields.Integer('código', size=20, required=True)
-	grupo = fields.Char('Grupo', size=100)
-	descripcion = fields.Char('Descripcion', size=200)
-	fechainicial = fields.Datetime('Fecha Inicial', size=20, required=True)
-	
-	@api.depends('fechainicial')
-	def _set_calculo_fecha(self):
-		for rec in self:
-			rec.fechafinal = rec.fechainicial+timedelta(days=30)
-			
-	fechafinal = fields.Datetime('Fecha Final', compute='_set_calculo_fecha')
-	votos = fields.Integer('Votos', size=20, readonly=True, compute='_calc_voto')
-	promedio = fields.Float('Promedio',size=10, readonly=True)
+    _name = "registro.ideas"
+    _description = "Tabla de ideas"
 
-	#sumar por cada idea las calificaciones obtenidas y promediar
-	def _calc_voto(self):
-		for rec in self.env['califica.ideas'].search([]):
-			domain=[
-				('name','=',rec.idea_id.id)
-					]
-			records = self.env['califica.ideas'].search(domain)
-			scored = records.mapped('calificacion_id')
-			rec.average=sum(scored)/len(rec)
+    name = fields.Char('Nombre de idea', size=256, required=True)
+    codigo = fields.Integer('código', size=20, required=True)
+    grupo = fields.Char('Grupo', size=100)
+    descripcion = fields.Char('Descripcion', size=200)
+    fechainicial = fields.Datetime('Fecha Inicial', size=20, required=True, default=date.today().strftime('%Y-%m-%d'))
 
+    @api.depends('fechainicial')
+    def _set_calculo_fecha(self):
+        for rec in self:
+            rec.fechafinal = rec.fechainicial+timedelta(days=30)
 
+    fechafinal = fields.Datetime('Fecha Final', compute='_set_calculo_fecha')
+    votos = fields.Float('Votos', size=20, readonly=True)
+    promedio = fields.Float('Promedio', size=10, readonly=True, compute='_calc_voto')
 
-    		
-    				
-    		
+    _sql_constraints = [
+                     ('codigo', 
+                      'unique(codigo)',
+                      'Choose another value - it has to be unique!')]
 
+#suma el campo 'calificacion_id' del modelo 'califica' agrupado por el campo 'codigo'
+    def _calc_voto(self):
+        for rec in self:
+            average_scored=0
+            scored_ids=self.env['califica.ideas'].search([('codigo','=',rec.codigo)])
+            if scored_ids:
+                number = len(scored_ids)
+                for scored in scored_ids:
+                    average_scored += scored.calificacion_id/number
 
-
-
-
-
-	
-
-
+        rec.promedio = average_scored
