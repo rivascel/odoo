207eaa50af20452cd129354f14b8e566bf3c092c
Jeison Pernia <jeisonestudio@gmail.com>
2021-07-23 13:13:37 -0400

    [REF] - Contrains cannot Duplicate qualification

--- ideas_app/models/califica.py
+++ ideas_app/models/califica.py
@@ -11,8 +11,12 @@ class califica_ideas (models.Model):
 	codigo = fields.Integer(string='codigo', related='name.codigo', store=True)
 	calificacion_id = fields.Float('calificacion', size=10, required=True)
 	fecha_actual = fields.Datetime('Fecha Calificacion', default=datetime.now(), readonly=True)
-	usuario_id= fields.Many2one('res.users', 'Usuario', readonly=True, default=lambda self: self.env.user)
-	
+	usuario_id = fields.Many2one('res.users', 'Usuario', readonly=True, default=lambda self: self.env.user)
+
+	_sql_constraints = [
+		('unique_qualification', 'unique(name, usuario_id)', 'You already rated this idea.'),
+	]
+
 	@api.constrains('calificacion_id')
 	def _calif(self):
 		for record in self:
@@ -20,10 +24,10 @@ class califica_ideas (models.Model):
 				raise exceptions.ValidationError('La calificación debe ser entre 0 y 10')
 	
 	#Se genene una restriccion para que el usuario solo puede calificar una sola vez una Idea 
-	@api.constrains('calificacion_id')
-	def _validaVoto(self):
-		for rec in self:
-			usuario_ideas_ids = self.search(['&',('usuario_id','=',rec.create_uid),('codigo','=',rec.codigo)])
-
-			if 'codigo' in 'usuario_ideas_ids' and 'usuario_id' in 'usuario_ideas_ids':
-				raise exceptions.ValidationError('Ya calificó esta idea')
+	# @api.constrains('calificacion_id')
+	# def _validaVoto(self):
+	# 	for rec in self:
+	# 		usuario_ideas_ids = self.search(['&',('usuario_id','=',rec.create_uid),('codigo','=',rec.codigo)])
+	#
+	# 		if 'codigo' in 'usuario_ideas_ids' and 'usuario_id' in 'usuario_ideas_ids':
+	# 			raise exceptions.ValidationError('Ya calificó esta idea')
