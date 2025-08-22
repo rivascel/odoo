6b6a9d27145e28f6b54d1f073e36d16d463826a0
Ivan <rivascel@gmail.com>
2025-08-21 20:05:44 -0500

    unificacion

--- ideas_app/ideas_app/models/grupo.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions
-
-class groupsUsers(models.Model):
-    _name = 'groups.users'
-    #_inherit = 'res.partner'
-    
-    id_usuario = fields.Many2one('res.partner','usuario')
-    grupo_ids = fields.Many2one('registro.grupos', string='Id Grupo', readonly=True)
